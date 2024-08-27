c5da52404c494c9eb0b5a8777801f43640b860fe
Oksana Tanashchuk <oksana.tanashuk@gmail.com>
2024-08-27 23:41:32 +0200

    added the first part of the project base

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+// import { CiCirclePlus } from "react-icons/ci"; 
+// import { HiArrowUpTray } from "react-icons/hi2"; 
+// import { HiOutlinePencilSquare } from "react-icons/hi2"; 
+function App() {
+  return <></>;
+}
+
+export default App;
