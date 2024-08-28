1fe73c3e03d4e25796a34cbd32858ddc39985722
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-08-28 22:49:59 +0300

    asdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import SharedLayout from "./components/SharedLayout/SharedLayout";
-import ModalWrapper from "./components/common/ModalWrapper/ModalWrapper";
-import { useState } from "react";
 
 function App() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
-
   return (
     <>
-      <button onClick={openModal}>Open modal</button>
-      <ModalWrapper modalIsOpen={isModalOpen} closeModal={closeModal}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum ipsam
-        error inventore quod dolorem, assumenda quo, impedit officiis nesciunt,
-        nisi dicta modi porro reprehenderit ea explicabo laborum? Beatae,
-        eligendi quas?
-      </ModalWrapper>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route path="/welcome" />
+          <Route path="/home" />
+          <Route path="/signup" />
+          <Route path="/signin" />
+        </Route>
+      </Routes>
     </>
-
-    // <Routes>
-    //   <Route path="/" element={<SharedLayout />}>
-    //     <Route path="/welcome" />
-    //     <Route path="/home" />
-    //     <Route path="/signup" />
-    //     <Route path="/signin" />
-    //   </Route>
-    // </Routes>
   );
 }
 
