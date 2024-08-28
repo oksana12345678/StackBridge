46492622809fed72fed4576fb1eab1980cc744c4
Oksana Tanashchuk <oksana.tanashuk@gmail.com>
2024-08-28 13:30:34 +0200

    remove  icon imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-// import { CiCirclePlus } from "react-icons/ci";
-// import { HiArrowUpTray } from "react-icons/hi2";
-// import { HiOutlinePencilSquare } from "react-icons/hi2";
 function App() {
   return (
     <>
