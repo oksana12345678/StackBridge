23e35db0bfe8d0bfc528264ac23abfdead13de42
Oksana Tanashchuk <oksana.tanashuk@gmail.com>
2024-08-28 13:28:23 +0200

    fix main.js clean App from Demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 // import { CiCirclePlus } from "react-icons/ci";
 // import { HiArrowUpTray } from "react-icons/hi2";
 // import { HiOutlinePencilSquare } from "react-icons/hi2";
-import Demo from "./components/common/demo";
 function App() {
   return (
     <>
-      <Demo />
       <></>
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -3,22 +3,21 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import "modern-normalize";
-import { PersistGate } from "redux-persist/integration/react";
-import { Provider } from "react-redux";
+// import { PersistGate } from "redux-persist/integration/react";
+// import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <Provider> */}
-      {/* <PersistGate loading={null}> */}
-        <BrowserRouter>
-          <HelmetProvider>
-            <App />
-          </HelmetProvider>
-        </BrowserRouter>
-      {/* </PersistGate> */}
+    {/* <PersistGate loading={null}> */}
+    <BrowserRouter>
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
+    </BrowserRouter>
+    {/* </PersistGate> */}
     {/* </Provider> */}
-    {/* <Provider>
   </StrictMode>
 );
