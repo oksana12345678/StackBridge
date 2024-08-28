aeabf8f2783f8be5a5c85ab7ae2438d2d5258fdf
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-08-28 13:38:24 +0300

    import modern normalize

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import "modern-normalize";
 // import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { BrowserRouter } from "react-router-dom";
