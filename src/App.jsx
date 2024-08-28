6f1f17d0f2e44926571c2b92c2ed13f36e08e27f
Oksana Tanashchuk <oksana.tanashuk@gmail.com>
2024-08-28 10:00:15 +0200

    added redux auth file also pages file

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-// import { CiCirclePlus } from "react-icons/ci"; 
-// import { HiArrowUpTray } from "react-icons/hi2"; 
-// import { HiOutlinePencilSquare } from "react-icons/hi2"; 
+// import { CiCirclePlus } from "react-icons/ci";
+// import { HiArrowUpTray } from "react-icons/hi2";
+// import { HiOutlinePencilSquare } from "react-icons/hi2";
 function App() {
-  return <></>;
+  return (
+    <>
+      <></>
+    </>
+  );
 }
 
 export default App;
