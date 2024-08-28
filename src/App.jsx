74f6a4ebf61de880c23ccb152ace30e37a5f3ecb
Oksana Tanashchuk <oksana.tanashuk@gmail.com>
2024-08-28 17:00:07 +0200

    change favicon added css reset added private route and shared layout added registration rout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import SharedLayout from "./components/SharedLayout/SharedLayout";
+
 function App() {
   return (
-    <>
-      <></>
-    </>
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route path="/welcome" />
+        <Route path="/home" />
+        <Route path="/signup" />
+        <Route path="/signin" />
+      </Route>
+    </Routes>
   );
 }
 
