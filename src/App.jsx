8b1825da8472d814759aefd252d9b1b984f746cf
pavlo <pavlohvozdetskyi@protonmail.com>
2024-08-29 19:55:48 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import SharedLayout from "./components/SharedLayout/SharedLayout";
-import {  useEffect } from "react";
+import { lazy, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useAuth } from "./hooks/userAuth.js";
 import { refreshUser } from "./redux/auth/operations.js";
 import RestrictedRoute from "./components/Route/RegisteredRoute/RegisteredRoute.jsx";
 import Loader from "./components/Loader/Loader.jsx";
-import SigninPage from "./pages/SignInPage/SignInPage.jsx";
+
+const SigninPage = lazy(() => import("./pages/SigninPage/SigninPage.jsx"));
 
 function App() {
   const dispatch = useDispatch();
@@ -27,7 +28,7 @@ function App() {
           <Route
             path="/signin"
             element={
-              <RestrictedRoute redirectTo="/home" component={<SigninPage />} />
+              <RestrictedRoute redirectTo="/home" component={SigninPage} />
             }
           />
         </Route>
