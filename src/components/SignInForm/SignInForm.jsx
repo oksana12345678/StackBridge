d45cea2273e93a44f5cbea34f98ddd24daac3fc8
pavlo <pavlohvozdetskyi@protonmail.com>
2024-08-31 00:44:51 +0300

    Update add eye-icon

--- src/components/SignInForm/SignInForm.jsx
+++ src/components/SignInForm/SignInForm.jsx
@@ -1,41 +1,103 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 import css from "./SignInForm.module.css";
-import { signinInputs } from "./SignInFormData.js";
-import { useForm } from "react-hook-form";
+import * as Yup from "yup";
+import { Field, Formik } from "formik";
+import { useDispatch } from "react-redux";
+import { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+
+const initialValues = {
+  email: "",
+  password: "",
+};
+
+const validationSchema = Yup.object({
+  email: Yup.string().email("Invalid email address").required("Required"),
+  password: Yup.string().required("Required"),
+});
 
 const SignInForm = () => {
-  const { register, handleSubmit, formState: { errors } } = useForm();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [showPassword, setShowPassword] = useState(false);
 
-  const onSubmit = (data) => {
-    console.log(data);
+  const handleSubmit = async ({ email, password }, { resetForm }) => {
+    dispatch(
+      logIn({
+        email,
+        password,
+      })
+    );
+    resetForm();
   };
 
   return (
     <div className={css.signinPageWraper}>
-      <form className={css.signinPageForm} onSubmit={handleSubmit(onSubmit)}>
-        <h4 className={css.signinPageTitle}>Sign In</h4>
-        {signinInputs.map((input, index) => (
-          <input
-            key={index}
-            label={input.label}
-            name={input.name}
-            placeholder={input.placeholder}
-            {...register(input.name)}
-            className={errors[input.name] ? css.error : ""}
-          />
-        ))}
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        {({ isSubmitting, touched, errors }) => (
+          <form className={css.form}>
+            <h4 className={css.signinPageTitle}>Sign In</h4>
+            <p className={css.label} htmlFor="email">
+              Enter your email
+            </p>
+            <Field
+              type="email"
+              name="email"
+              id="email"
+              placeholder="E-mail"
+              className={touched.email && errors.email ? css.inputError : ""}
+              autoComplete="email"
+            />
+            {touched.email && errors.email && (
+              <div className={css.errorMsg}>{errors.email}</div>
+            )}
 
-        <button className={css.signinPageButton} type="submit">
-          Sign In
-        </button>
-        <Link className={css.signinPageLink} to={"/forgot-password"}>
-          Forgot password?
-        </Link>
-        <Link className={css.signinPageLink} to={"/signup"}>
-          Sign up
-        </Link>
-      </form>
+            <p className={css.label} htmlFor="password">
+              Enter your password
+              <button
+                className={css.eyeBtn}
+                onClick={(event) => {
+                  event.preventDefault();
+                  setShowPassword(!showPassword);
+                }}
+              >
+                {showPassword ? (
+                  <FaEye className={css.faEye} />
+                ) : (
+                  <FaEyeSlash className={css.faEyeSlash} />
+                )}
+              </button>
+            </p>
+            <Field
+              type={showPassword ? "text" : "password"}
+              name="password"
+              id="password"
+              placeholder="Password"
+              className={
+                touched.password && errors.password ? css.inputError : ""
+              }
+              autoComplete="current-password"
+            />
+            {touched.password && errors.password && (
+              <div className={css.errorMsg}>{errors.password}</div>
+            )}
 
+            <button className={css.signinPageButton} type="submit">
+              Sign In
+            </button>
+            <NavLink className={css.signinPageLink} to={"/forgot-password"}>
+              Forgot password?
+            </NavLink>
+            <NavLink className={css.signinPageLink} to={"/signup"}>
+              Sign up
+            </NavLink>
+          </form>
+        )}
+      </Formik>
       <div className={css.signinPageBottle}></div>
     </div>
   );
