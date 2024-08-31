ba031cadda1ddbfbd36add8697fbd01de9ab7fd3
Shnurkova <Annshnur@gmail.com>
2024-08-31 17:27:30 +0300

    delete import and element

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import SharedLayout from "./components/SharedLayout/SharedLayout";
-import WelcomePage from './pages/WelcomePage/WelcomePage';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<SharedLayout />}>
-        <Route path="/welcome" element={<WelcomePage />}/>
+        <Route path="/welcome"/>
         <Route path="/home" />
         <Route path="/signup" />
         <Route path="/signin" />
