cb91f9dcc260d843b2db9d8e4305373b3266e8bb
Oksana Tanashchuk <oksana.tanashuk@gmail.com>
2024-08-31 12:25:33 +0200

    changed the icon of not logged in changed the path to the icon of logged in added to render the username

--- src/components/Header/UserLogo/UserLogo.jsx
+++ src/components/Header/UserLogo/UserLogo.jsx
@@ -1,52 +1,38 @@
 import { useSelector } from "react-redux";
 import css from "./UserLogo.module.css";
 import { useDispatch } from "react-redux";
-// import { selectUsers } from "../../../redux/service/user/getCurrentInfo/selectors";
 import { userLogoModal } from "../../../redux/modalWindow/slice";
 import { selectIsUserLogoModalOpen } from "../../../redux/modalWindow/selectors";
 import UserLogoPopUp from "../UserLogoPopUp/UserLogoPopUp";
 import { useRef } from "react";
 import { IoIosArrowUp } from "react-icons/io";
 import { IoIosArrowDown } from "react-icons/io";
+import { selectUserEmail } from "../../../redux/auth/selectors";
 
 export const UserLogo = () => {
-  // const userProfile = useSelector(selectUsers);
+  const userProfile = useSelector(selectUserEmail);
   const buttonNode = useRef();
   const dispatch = useDispatch();
   const isUserLogoModalOpen = useSelector(selectIsUserLogoModalOpen);
-  const defaultUserImage = "https://avatar.iran.liara.run/public/6";
+  const defaultUserImage = "../../../../public/userPic.png";
+  const defaultName = "User Name";
 
   const onClickOpenUserLogoModal = () => {
     dispatch(userLogoModal());
   };
 
-  // const name = userProfile.name;
-  // const email = userProfile.email;
-  // const userAvatar = userProfile.avatar;
-  // const enteredUserEmail = emailUsername(email);
-
-  // function emailUsername(emailAddress) {
-  //   return emailAddress.split("@")[0];
-  // }
-
-  // const makeUserName = () => {
-  //   if (name) {
-  //     return name;
-  //   }
-  //   if (!name && email) {
-  //     return enteredUserEmail || "";
-  //   } else {
-  //     return "User Name";
-  //   }
-  // };
+  const name = userProfile.name;
+  const userAvatar = userProfile.avatar;
 
   return (
     <div onClick={onClickOpenUserLogoModal} ref={buttonNode}>
       <button className={css.button} aria-label="User Logo">
-        {/* <p>{name ? name : makeUserName}</p> */}
-        <p className={css.name}>david</p>
+        <p className={css.name}>{name ? name : defaultName}</p>
 
-        <img className={css.userAvatar} src={defaultUserImage} />
+        <img
+          className={css.userAvatar}
+          src={userAvatar ? userAvatar : defaultUserImage}
+        />
 
         <div className={css.icon}>
           {isUserLogoModalOpen ? (
