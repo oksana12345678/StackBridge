f76c98905d316e2f19296a7b407904ab6cbe9196
tina3008 <kicja_3@ukr.net>
2024-08-31 13:12:18 +0300

    validation

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -7,20 +7,24 @@ import { useDispatch } from "react-redux";
 import { register } from "../../redux/auth/operations";
 import css from "./SignUpForm.module.css";
 import fieldEye from "./fieldEye";
+import { HiOutlineEye, HiOutlineEyeOff } from "react-icons/hi";
 
 export default function SignUpForm() {
   const dispatch = useDispatch();
 
-  //   const validationControl = Yup.object().shape({
-  //     email: Yup.string()
-  //       .min(3, "Too Short!")
-  //       .max(50, "Too Long!")
-  //       .required("Required"),
-  //     password: Yup.string()
-  //       .min(5, "Too short")
-  //       .max(18, "Too long")
-  //       .required("Required"),
-  //   });
+  const validationControl = Yup.object().shape({
+    email: Yup.string()
+      .min(8, "Too Short!")
+      .max(64, "Too Long!")
+      .required("Required"),
+    password: Yup.string()
+      .min(8, "Too short")
+      .max(64, "Too long")
+      .required("Required"),
+    repeatPassword: Yup.string()
+      .oneOf([Yup.ref("password"), null], "Password don't match")
+      .required("Required"),
+  });
 
   const handleSubmit = (values, actions) => {
     // dispatch(register(values))
@@ -32,11 +36,9 @@ export default function SignUpForm() {
       register({
         email,
         password,
-       
       })
     );
     actions.resetForm();
-    
   };
 
   return (
@@ -48,35 +50,56 @@ export default function SignUpForm() {
           password: "",
           repeatPassword: "",
         }}
-        validate={(values) => {
-          const errors = {};
-          if (values.password !== values.repeatPassword) {
-            errors.repeatPassword = "Password don't match";
-          }
-          return errors;
-        }}
-        onSubmit={(handleSubmit)}
+        // validate={(values) => {
+        //   const errors = {};
+        //   if (values.password !== values.repeatPassword) {
+        //     errors.repeatPassword = "Password don't match";
+        //   }
+        //   return errors;
+        // }}
+        validationSchema={validationControl}
+        onSubmit={handleSubmit}
       >
         <Form className={css.form} autoComplete="off">
           <div className={css.fialdStyle}>
-            <label className={css.label}>
-              Enter your email
+            <div>
+              <label className={css.label}>Enter your email</label>
+              {/* {ErrorMessage  ? (
+                <Field
+                  type="email"
+                  name="email"
+                  className={css.errField}
+                  placeholder="E-mail"
+                />
+              ) : ( */}
               <Field
                 type="email"
                 name="email"
                 className={css.field}
                 placeholder="E-mail"
               />
-            </label>
-            <label className={css.label}>
-              Enter your password
+              <HiOutlineEyeOff />
+
+              <ErrorMessage
+                className={css.msgErr}
+                name="email"
+                component="span"
+              />
+            </div>
+            <div>
+              <label className={css.label}>Enter your password</label>
               <Field
                 type="password"
                 name="password"
                 className={css.field}
                 placeholder="Password"
               />
-            </label>
+              <ErrorMessage
+                className={css.msgErr}
+                name="password"
+                component="span"
+              />
+            </div>
             <div>
               <label className={css.label}>Repeat password</label>
               <Field
@@ -85,8 +108,13 @@ export default function SignUpForm() {
                 placeholder="Repeat password"
                 className={css.field}
               />
-              <ErrorMessage name="repeatPassword" component="span" />
+              <ErrorMessage
+                className={css.msgErr}
+                name="repeatPassword"
+                component="span"
+              />
             </div>
+
             <button type="submit" className={css.btn}>
               Sign Up
             </button>
