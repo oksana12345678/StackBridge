74a43fce051ce9a20b9cb921eb8421280b8bbac7
tina3008 <kicja_3@ukr.net>
2024-08-31 18:29:56 +0300

    singup

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -2,74 +2,140 @@ import { Formik, Form, Field, ErrorMessage } from "formik";
 import { NavLink } from "react-router-dom";
 import { useId } from "react";
 import * as Yup from "yup";
-// import toast from "react-hot-toast";
-// import { useDispatch } from "react-redux";
-// import { register } from "../../redux/auth/operations";
+import { toast, ToastContainer } from "react-toastify";
+import { useDispatch } from "react-redux";
+import { register } from "../../redux/auth/operations";
 import css from "./SignUpForm.module.css";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+// import { HiOutlineEye, HiOutlineEyeOff } from "react-icons/hi";
+import React, { useState } from "react";
 
 export default function SignUpForm() {
-//   const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  const notify = () => {
+    toast("");
+  };
+  const validationControl = Yup.object().shape({
+    email: Yup.string()
+      .min(8, "Too Short!")
+      .max(64, "Too Long!")
+      .required("Required"),
+    password: Yup.string()
+      .min(8, "Too short")
+      .max(64, "Too long")
+      .required("Required"),
+    repeatPassword: Yup.string()
+      .oneOf([Yup.ref("password"), null], "Password don't match")
+      .required("Required"),
+  });
 
-//   const validationControl = Yup.object().shape({
-//     email: Yup.string()
-//       .min(3, "Too Short!")
-//       .max(50, "Too Long!")
-//       .required("Required"),
-//     password: Yup.string()
-//       .min(5, "Too short")
-//       .max(18, "Too long")
-//       .required("Required"),
-//   });
+  const handleSubmit = (values, actions) => {
 
-//   const handleSubmit = (values, actions) => {
-//     dispatch(register(values))
-//       .unwrap()
-//       .then((data) => console.log(data))
-//       .catch((err) => console.log(err));
+    const { email, password } = values;
+    dispatch(
+      register({
+        email,
+        password,
+      })
+    );
+    actions.resetForm();
+  };
+  const [showPassword, setShowPassword] = useState(false);
+const handleTogglePassword = (fieldName) => {
+  if (fieldName === "password" || fieldName === "repeatPassword") {
+    setShowPassword(!showPassword);
+  }
+};
 
-//     actions.resetForm();
-//   };
 
   return (
     <div className={css.form_box}>
+      <ToastContainer />
       <h2 className={css.title}>Sign Up</h2>
+
       <Formik
         initialValues={{
-          name: "",
           email: "",
           password: "",
+          repeatPassword: "",
         }}
-        //   onSubmit={handleSubmit}
+        validationSchema={validationControl}
+        onSubmit={handleSubmit}
       >
         <Form className={css.form} autoComplete="off">
           <div className={css.fialdStyle}>
-            <label className={css.label}>
-              Enter your email
+            <div>
+              <label className={css.label}>Enter your email</label>
+
               <Field
                 type="email"
                 name="email"
                 className={css.field}
                 placeholder="E-mail"
               />
-            </label>
-            <label className={css.label}>
-              Enter your password
+              <ErrorMessage
+                className={css.msgErr}
+                name="email"
+                component="span"
+              />
+            </div>
+            <div>
+              <label className={css.label}>Enter your password</label>
               <Field
-                type="password"
+                type={showPassword ? "text" : "password"}
                 name="password"
+                id="password"
+
                 className={css.field}
                 placeholder="Password"
               />
-            </label>
-            <label className={css.label}>
-              Repeat password
+
+              <button
+                type="button"
+                onClick={() => handleTogglePassword("password")}
+                // onClick={() => setShowPassword(!showPassword)}
+                className="eye"
+              >
+                {showPassword ? (
+                  <FaEyeSlash size="16" className="css.eye" />
+                ) : (
+                  <FaEye size="16" className="css.eye" />
+                )}
+              </button>
+              <ErrorMessage
+                className={css.msgErr}
+                name="password"
+                component="span"
+              />
+            </div>
+            <div>
+              <label className={css.label}>Repeat password</label>
               <Field
-                type="password"
-                name="passwordRepeat"
+                type={showPassword ? "text" : "password"}
+                name="repeatPassword"
+                id="repeatPassword"
                 placeholder="Repeat password"
                 className={css.field}
               />
-            </label>
+              <button
+                type="button"
+                // onClick={() => setShowPassword(!showPassword)}
+                onClick={() => handleTogglePassword("repeatPassword")}
+                className="eye"
+              >
+                {showPassword ? (
+                  <FaEyeSlash size="16" className="css.eye" />
+                ) : (
+                  <FaEye size="16" className="css.eye" />
+                )}
+              </button>
+              <ErrorMessage
+                className={css.msgErr}
+                name="repeatPassword"
+                component="span"
+              />
+            </div>
+
             <button type="submit" className={css.btn}>
               Sign Up
             </button>
@@ -81,7 +147,6 @@ export default function SignUpForm() {
           Sign in
         </NavLink>
       </nav>
-
     </div>
   );
 }
--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -2,16 +2,15 @@ import { Helmet } from "react-helmet-async";
 // import clsx from "clsx";
 // import Image from "next/image";
 
-import SignUpForm from "../../components/SignUpForm/SignUpForm.jsx";
 import css from "./SignupPage.module.css";
+import SignUpForm from "../../components/SignUpForm/SignUpForm";
 
-export default function RegisterPage() {
+export default function SignupPage() {
   return (
     <>
       <Helmet>
         <title>Sign up page</title>
       </Helmet>
-
       <div className={css.div}>
         <SignUpForm />
       </div>
