d674d3ec4091353fa4dfe434fb924644191987d9
Oksana Tanashchuk <oksana.tanashuk@gmail.com>
2024-08-31 09:46:22 +0200

    add tost message change

---
+++ src/components/showToast.js
@@ -0,0 +1,16 @@
+import { toast } from "react-toastify";
+
+const showToast = (message, type) => {
+  toast(message, {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: type === "success" ? "light" : "colored",
+    type: type,
+  });
+};
+export default showToast;
