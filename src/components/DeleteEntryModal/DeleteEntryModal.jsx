6a0fc0b52422ff67b5989beab0b02e872286387c
PRO100 Cliry <128536194+Cliry1@users.noreply.github.com>
2024-08-31 17:17:04 +0300

    Update DeleteEntryModal.jsx

--- src/components/DeleteEntryModal/DeleteEntryModal.jsx
+++ src/components/DeleteEntryModal/DeleteEntryModal.jsx
@@ -19,7 +19,7 @@ export default function DeleteEntryModal() {
           <h3 className={css.text}>Are you sure you want to delete the entry?</h3>
           <div className={css.modalButtons}>
             <button className={css.buttonCancel} onClick={() => dispatch(closeModal())}>Cancel</button>
-            <button className={css.buttonLogout} onClick={()=>{dispatch(logOut())}}>Delete</button>
+            <button className={css.buttonLogout} >Delete</button>
           </div>
         </div>
       </ModalWrapper>
