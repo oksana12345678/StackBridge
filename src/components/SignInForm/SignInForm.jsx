c29b3a86e5a3f78cff33f38718e6a5e556c48730
pavlo <pavlohvozdetskyi@protonmail.com>
2024-09-01 22:38:04 +0300

    Updated Error Msg Gap

--- src/components/SignInForm/SignInForm.jsx
+++ src/components/SignInForm/SignInForm.jsx
@@ -66,53 +66,57 @@ const SignInForm = () => {
             <label className={css.label} htmlFor="email">
               Enter your email
             </label>
-            <Field
-              type="email"
-              name="email"
-              id="email"
-              placeholder="Enter your email"
-              className={`${css.input} ${
-                errors.email && touched.email ? css.inputError : ""
-              }`}
-            />
-            <ErrorMessage
-              className={css.errorMsg}
-              name="email"
-              component="span"
-            />
+            <div className={css.inputErrorContainer}>
+              <Field
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Enter your email"
+                className={`${css.input} ${
+                  errors.email && touched.email ? css.inputError : ""
+                }`}
+              />
+              <ErrorMessage
+                className={css.errorMsg}
+                name="email"
+                component="span"
+              />
+            </div>
 
             <label className={css.label} htmlFor="password">
               Enter your password
             </label>
-            <div className={css.passwordWrapper}>
-              <Field
-                type={showPassword ? "text" : "password"}
+            <div className={css.inputErrorContainer}>
+              <div className={css.passwordWrapper}>
+                <Field
+                  type={showPassword ? "text" : "password"}
+                  name="password"
+                  id="password"
+                  placeholder="Enter your password"
+                  className={`${css.input} ${
+                    errors.password && touched.password ? css.inputError : ""
+                  }`}
+                />
+                <button
+                  className={css.eyeBtn}
+                  onClick={(event) => {
+                    event.preventDefault();
+                    setShowPassword(!showPassword);
+                  }}
+                >
+                  {showPassword ? (
+                    <HiOutlineEye className={css.faEye} />
+                  ) : (
+                    <HiOutlineEyeOff className={css.faEye} />
+                  )}
+                </button>
+              </div>
+              <ErrorMessage
+                className={css.errorMsg}
                 name="password"
-                id="password"
-                placeholder="Enter your password"
-                className={`${css.input} ${
-                  errors.password && touched.password ? css.inputError : ""
-                }`}
+                component="span"
               />
-              <button
-                className={css.eyeBtn}
-                onClick={(event) => {
-                  event.preventDefault();
-                  setShowPassword(!showPassword);
-                }}
-              >
-                {showPassword ? (
-                  <HiOutlineEye className={css.faEye} />
-                ) : (
-                  <HiOutlineEyeOff className={css.faEye} />
-                )}
-              </button>
             </div>
-            <ErrorMessage
-              className={css.errorMsg}
-              name="password"
-              component="span"
-            />
 
             <button className={css.signinPageButton} type="submit">
               Sign In
