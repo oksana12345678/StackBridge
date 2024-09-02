97b82f24de040f3c4edd285f0a39e8b969007706
Antonina Grebeniuk <rebel.t2808@gmail.com>
2024-09-02 20:29:37 +0400

    add closing modal window on timer

--- src/redux/monthStats/selectors.js
+++ src/redux/monthStats/selectors.js
@@ -1,6 +1,4 @@
 export const selectCurrentMonth = state => state.monthStats.currentMonth;
 export const selectCurrentYear = state => state.monthStats.currentYear;
 export const selectDaysStats = state => state.monthStats.daysStats;
-// export const selectLoading = state => state.monthStats.loading;
 export const selectSelectedDay = state => state.monthStats.selectedDay;
-export const selectIsOpen = state => state.monthStats.isOpen;
