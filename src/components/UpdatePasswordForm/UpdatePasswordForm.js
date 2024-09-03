22b9545a8aa3f9a52fd31701e8b83edfa9b3cf74
Anna Samtash <sansara6900@gmail.com>
2024-09-03 19:43:09 +0300

    delete updatePasswordForm.js

--- src/components/UpdatePasswordForm/UpdatePasswordForm.js
+++
@@ -1,9 +0,0 @@
-export const UpdatePassword = async (userData) => {
-  try {
-    const response = await axios.post("auth/update-password", userData);
-
-    return response.data;
-  } catch (error) {
-    console.log(error.message);
-  }
-};
