c4cb787cda9c5e26f47cec3fb8e3414e5d681a45
Melnyk Roman <melnikroman2005@ukr.net>
2024-09-04 19:28:12 +0300

    WaterRatioPanel

--- src/components/WaterRatioPanel/WaterRatioPanel.jsx
+++ src/components/WaterRatioPanel/WaterRatioPanel.jsx
@@ -1,81 +1,65 @@
-import { useState, useEffect, useMemo, useRef } from "react";
-import css from "./WaterRatioPanel.module.css";
 import { useSelector } from "react-redux";
-import { selectWatersToday } from "../../redux/waterConsumption/selectors";
-// import TodayListModal from "../../components/TodayListModal/TodayListModal";
 import { HiOutlinePlusCircle as Plus } from "react-icons/hi2";
 
-const mockWaterToday = { progress: "15" };
+import { selectWatersToday } from "../../redux/waterConsumption/selectors.js";
+
+import css from "./WaterRatioPanel.module.css";
 
 const WaterRatioPanel = () => {
-  const waterToday = mockWaterToday; // useSelector(selectWatersToday)
+  const waterConsumption = useSelector(selectWatersToday);
 
-  const [progressValue, setProgressValue] = useState(0);
-  const [previousValue, setPreviousValue] = useState(progressValue || 0);
-  const [isOpen, setIsOpen] = useState(false);
-
-  useEffect(() => {
-    const value = Math.min(100, Number(parseInt(waterToday?.progress, 10)));
-    setProgressValue(value);
-  }, [waterToday]);
-
-  const openModal = () => {
-    setIsOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsOpen(false);
-  };
-
-  const renderLines = useMemo(() => {
-    return (
-      <>
-        <span className={css.line}></span>
-        <span className={css.line}></span>
-        <span className={css.line}></span>
-      </>
-    );
-  }, [progressValue]);
-
-  const renderPercentages = useMemo(() => {
-    return (
-      <>
-        <span className={css.number1}>0%</span>
-        <span className={css.number2}>50%</span>
-        <span className={css.number3}>100%</span>
-      </>
-    );
-  }, [progressValue]);
+  const progressValue = waterConsumption?.percentOfWaterRate || "0%";
 
   return (
-    <div>
-      <h2 className={css.today}>Today</h2>
-      <div className={css.mainContainer}>
-        <div className={css.panel}>
-          <div className={css.range}>
-            <div className={css.rangeContent}>
-              <input
-                type="range"
-                min="0"
-                max="100"
-                value={progressValue}
-                step="1"
-                readOnly
-              />
+    <div className={css.waterRatioPanelWrapper}>
+      <div className={css.progressWrapper}>
+        <p className={css.progressTitle}>Today</p>
+        <div className={css.progressBarWrapper}>
+          <div className={css.progressTrack}>
+            <div
+              className={css.progressTrackValue}
+              style={{
+                width: `${progressValue}`,
+              }}
+            ></div>
+            <div className={css.adjustmentWrapper}>
+              <div
+                className={css.progressSlider}
+                style={{
+                  left: `${progressValue}`,
+                }}
+              ></div>
             </div>
           </div>
-
-          <div className={css.decorativeLines}>{renderLines}</div>
-          <div className={css.percentages}>{renderPercentages}</div>
+          <div className={css.progressBarRangeScale}>
+            <span
+              className={`${css.scaleValue} ${
+                progressValue <= 0 ? css.activeScaleValue : ""
+              }`}
+            >
+              0%
+            </span>
+            <span
+              className={`${css.scaleValue} ${
+                progressValue === 50 ? css.activeScaleValue : ""
+              }`}
+            >
+              50%
+            </span>
+            <span
+              className={`${css.scaleValue} ${
+                progressValue >= 100 ? css.activeScaleValue : ""
+              }`}
+            >
+              100%
+            </span>
+          </div>
         </div>
-
-        <button type="button" className={css.addButton} onClick={openModal}>
-          <Plus className={css.plusCircle} size={24} />
-          <span className={css.addButtonText}>Add Water</span>
-        </button>
       </div>
-
-      {/* {isOpen && <TodayListModal />} */}
+      <button type="button" className={css.addButton}>
+        <Plus className={css.plusCircle} size={24} />
+        <span className={css.addButtonText}>Add Water</span>
+      </button>
     </div>
   );
 };
