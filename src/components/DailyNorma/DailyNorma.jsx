769ab06b7978bee14216d1685751d5d3f5d137d7
Melnyk Roman <melnikroman2005@ukr.net>
2024-09-05 16:59:34 +0300

    Fix-DailyNorma-and-add-opening-modal-window-in-WaterRatioPanel

--- src/components/DailyNorma/DailyNorma.jsx
+++ src/components/DailyNorma/DailyNorma.jsx
@@ -1,19 +1,55 @@
-import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Hourglass } from "react-loader-spinner";
+
+import {
+  selectError,
+  selectLoading,
+  selectWaterRate,
+} from "../../redux/dailyNormalModal/selectors.js";
+import { fetchUserData } from "../../redux/dailyNormalModal/operations.js";
+
+import showToast from "../showToast.js";
+
 import css from "./DailyNorma.module.css";
-import { selectWaterRate } from "../../redux/dailyNormalModal/selectors";
 
 const DailyNorma = ({ handleOpenModal }) => {
-  const dailyNorma = useSelector(selectWaterRate);
+  const dispatch = useDispatch();
+
+  const waterRate = useSelector(selectWaterRate);
+  const loading = useSelector(selectLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchUserData());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (error) {
+      showToast("Error with loading your daily norma!", "error");
+    }
+  }, [error]);
 
   return (
     <div className={css.dailyNormaContainer}>
       <div className={css.title}>My daily norma</div>
       <div className={css.infoContainer}>
-        {dailyNorma ? dailyNorma / 1000 : 2} L{" "}
-        <button
-          className={css.editBtn}
-          onClick={handleOpenModal}
-        >
+        {loading ? (
+          <Hourglass
+            visible={true}
+            height="20"
+            width="20"
+            ariaLabel="hourglass-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+            colors={["#306cce", "#72a1ed"]}
+          />
+        ) : error ? (
+          <div>2 L</div>
+        ) : (
+          <div>{waterRate ? waterRate / 1000 : 2} L</div>
+        )}
+        <button className={css.editBtn} onClick={handleOpenModal}>
           Edit
         </button>
       </div>
--- src/redux/dailyNormalModal/selectors.js
+++ src/redux/dailyNormalModal/selectors.js
@@ -1,2 +1,5 @@
-export const selectWaterRate = (state) =>
-  state.dailyNormalModal.waterRate;
+export const selectWaterRate = (state) => state.dailyNormalModal.waterRate;
+
+export const selectLoading = (state) => state.dailyNormalModal.loading;
+
+export const selectError = (state) => state.dailyNormalModal.error;
