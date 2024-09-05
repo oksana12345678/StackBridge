55bf9834098e26cab16aaa70aca048285ab42c39
Oksana Tanashchuk <oksana.tanashuk@gmail.com>
2024-09-05 09:31:52 +0200

    fixed issues with header  popup

--- src/components/DaysGeneralStats/DaysGeneralStats.jsx
+++ src/components/DaysGeneralStats/DaysGeneralStats.jsx
@@ -29,11 +29,11 @@ const DaysGeneralStats = ({
         Daily norma: <span className={css.statsInfoValue}>{dailyNorma}</span>
       </div>
       <div className={css.statsInfo}>
-        Fulfillment of the daily norm:{" "}
+        Fulfillment of the daily norm:
         <span className={css.statsInfoValue}>{percentageOfFulfillment}</span>
       </div>
       <div className={css.statsInfo}>
-        How many servings of water:{" "}
+        How many servings of water:
         <span className={css.statsInfoValue}>{servingsAmount}</span>
       </div>
     </div>
