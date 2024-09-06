8b3040110926cce1576fd9ca061f410b8ad1a417
Oksana Tanashchuk <oksana.tanashuk@gmail.com>
2024-09-06 22:11:22 +0200

    change auto-close

--- src/components/showToast.js
+++ src/components/showToast.js
@@ -3,7 +3,7 @@ import { toast } from "react-toastify";
 const showToast = (message, type) => {
   toast(message, {
     position: "top-right",
-    autoClose: 5000,
+    autoClose: 3000,
     hideProgressBar: false,
     closeOnClick: true,
     pauseOnHover: true,
