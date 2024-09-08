da876ff9ec7528037c605a69ea65abf1d073c334
Oksana Tanashchuk <oksana.tanashuk@gmail.com>
2024-09-08 11:37:08 +0200

    fix z-index on header

--- src/redux/user/selectors.js
+++
@@ -1,3 +0,0 @@
-export const selectUserGender = (state) => state.user.gender;
-export const selectWaterRateUpdate = (state) => state.user.waterRate;
-export const selectLoadingUserData = (state) => state.user.loading;
