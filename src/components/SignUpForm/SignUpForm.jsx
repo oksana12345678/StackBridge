ecf7195749b070bff0e69e1332493f1c14c18d76
tina3008 <kicja_3@ukr.net>
2024-09-10 15:58:15 +0300

    loader&750welcomepage-backgrnd

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -1,13 +1,15 @@
 import * as Yup from "yup";
 import { useDispatch } from "react-redux";
-
 import { register } from "../../redux/auth/operations.js";
 import { SignInSignUpWrapper } from "../SignInSignUpWrapper/SignInSignUpWrapper.jsx";
 import showToast from "../showToast.js";
+import Loader from '../Loader/Loader.jsx'
+import { useState } from "react";
 
 const SignUpForm = () => {
   const dispatch = useDispatch();
-
+  const [isLoading, setIsLoading] = useState(false);
+  
   const validationControl = Yup.object().shape({
     email: Yup.string().email("Invalid email address").required("Required"),
     password: Yup.string()
@@ -21,6 +23,7 @@ const SignUpForm = () => {
 
   const handleSubmit = (values) => {
     const { email, password } = values;
+    setIsLoading(true);
     dispatch(
       register({
         email,
@@ -33,14 +36,23 @@ const SignUpForm = () => {
       })
       .catch(() => {
         showToast("Incorrect login or password", "error");
+      })
+      .finally(() => {
+        setIsLoading(false); 
       });
   };
 
   return (
-    <SignInSignUpWrapper
-      contactsSchema={validationControl}
-      handleSubmit={handleSubmit}
-    />
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <SignInSignUpWrapper
+          contactsSchema={validationControl}
+          handleSubmit={handleSubmit}
+        />
+      )}
+    </>
   );
 };
 
