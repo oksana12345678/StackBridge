3f365093dbfbd5fdb648d6f07858c936bffab6ed
Oksana Tanashchuk <oksana.tanashuk@gmail.com>
2024-09-20 09:12:46 +0200

    Revert "Merge branch 'main' of https://github.com/oksana12345678/StackBridge"
    This reverts commit 91265fba9f58f7113a7dfc8614070032f2eb839c, reversing
    changes made to db3b24cdf73ee8ffa13180af0e5a6ba16f8cb47e.

--- src/components/UpdatePasswordForm/UpdatePasswordApi.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export const UpdatePassword = async (userData) => {
-  const response = await axios.post("auth/update-password", userData);
-
-  return response.data;
-};
